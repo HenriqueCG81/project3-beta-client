b0d9218eeed0dc285855c3cb434dc0bb8d340d3e
HenriqueCG81 <123783235+HenriqueCG81@users.noreply.github.com>
2023-07-14 11:24:43 +0100

    second update

--- src/pages/admin/Doctors.js
+++ src/pages/admin/Doctors.js
@@ -1,7 +1,7 @@
 import Layout from '../../components/Layout';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Table, message } from 'antd';
 const Doctors = () => {
   const [doctors, setDoctors] = useState([]);
 
@@ -19,12 +19,72 @@ const Doctors = () => {
       console.log(error);
     }
   };
+  const handleAccountStatus = async (record, status) => {
+    try {
+      const res = await axios.post(
+        '/api/v1/admin/changeAccountStatus',
+        { doctorId: record._id, userId: record.userId, status: status },
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem('token')}`
+          }
+        }
+      );
+      if (res.data.success) {
+        message.success(res.data.message);
+        window.location.reload();
+      }
+    } catch (error) {
+      message.error('Something went wrong');
+    }
+  };
   useEffect(() => {
     getDoctors();
   }, []);
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      render: (text, record) => (
+        <span>
+          {record.firstName}
+          {record.lastName}
+        </span>
+      )
+    },
+    {
+      title: 'Status',
+      dataIndex: 'status'
+    },
+    {
+      title: 'Phone',
+      dataIndex: 'phone'
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      render: (text, record) => (
+        <div className="d-flex" key={record.status}>
+          {record.status === 'pending' ? (
+            <button
+              className="btn btn-success"
+              onClick={() => handleAccountStatus(record, 'approved')}
+            >
+              Approve
+            </button>
+          ) : (
+            <button className="btn btn-danger">Reject</button>
+          )}
+        </div>
+      )
+    }
+  ];
+
   return (
     <Layout>
-      <h1>All Doctors</h1>
+      <h1 className="text-center m-2">All Doctors</h1>
+      <Table columns={columns} dataSource={doctors} />
     </Layout>
   );
 };
